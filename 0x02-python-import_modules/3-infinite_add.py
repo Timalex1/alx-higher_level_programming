5d493de3e940042bc48116dec5202eb37141f10c
Timalex1 <timilehinvincent@gmail.com>
2021-09-09 04:16:29 -0700

    Adding files

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
     res = 0
     for arg in argv[1:]:
-        res +=int(arg)
+        res += int(arg)
     print("{:d}".format(res))
