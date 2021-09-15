0b8bd4caadf7e069cf1263410598f66629c129d4
Timalex1 <timilehinvincent@gmail.com>
2021-09-15 05:26:31 -0700

    Adding files

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,8 +6,8 @@ def weight_average(my_list=[]):
     average = 0
     result = 0
     for i in my_list:
-        result += i[1] * i[2]
+        result += i[0] * i[1]
 
-        average += i[2]
+        average += i[1]
 
     return result / average
