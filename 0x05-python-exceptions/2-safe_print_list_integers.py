8d8fec8c9be916f360ddb341b4f8492414e604cd
Timalex1 <timilehinvincent@gmail.com>
2021-09-28 05:11:44 -0700

    Adding file to git

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-    for i in my_list:
-        try: 
-            if num < x:
-                print("{:d}".format(i), end='')
-                num += 1
+    for i in range(num, x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            num += 1
         except (ValueError, TypeError):
             pass
     print()
