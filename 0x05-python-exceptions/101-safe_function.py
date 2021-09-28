e0da95f811bcb0a7625160d4276a9b59d8582adc
Timalex1 <timilehinvincent@gmail.com>
2021-09-28 06:12:36 -0700

    Fixing files

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+    except Exception as ex:
+        print('Exception: {}'.format(ex))
+        return False
+    return True
