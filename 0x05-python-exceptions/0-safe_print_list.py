6eef10a58c142f800d03e8489f54cc50f23cf067
Timalex1 <timilehinvincent@gmail.com>
2021-09-28 04:35:12 -0700

    Fixing error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
     try:
         num = 0
         for i in my_list:
@@ -9,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
                 num += 1
         print('')
         return num
-
     except SyntaxError:
         print("Error Syntax")
