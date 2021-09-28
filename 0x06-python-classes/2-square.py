69a55f090acdcd84b815a237bd53aa15a60a113d
Timalex1 <timilehinvincent@gmail.com>
2021-09-28 07:42:55 -0700

    Adding files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,9 @@
 class Square:
     """ Square class """
     def __init__(self, size=0):
-        if type(size) !- int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise VallueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
