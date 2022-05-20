eefc949854ccd875d36ac1485a6b4607842b3972
Timalex1 <timilehinvincent@gmail.com>
2022-05-20 09:05:09 +0000

    fixing errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,22 +4,22 @@ const arrLength = intArray.length;
 let result, i, j;
 
 function secondLargest (intArray) {
-    result = intArray.slice(0);
-    i = Math.max.apply(Math, result);
-    i = i - 1;
+  result = intArray.slice(0);
+  i = Math.max.apply(Math, result);
+  i = i - 1;
 
-    while (i > 0) {
-	for (j = 0; j < arrLength; j++) {
-	    if (parseInt(i) === parseInt(result[j])) {
-		return result[j];
-	    }
-	}
-	i--;
+  while (i > 0) {
+    for (j = 0; j < arrLength; j++) {
+      if (parseInt(i) === parseInt(result[j])) {
+        return result[j];
+      }
     }
+    i--;
+  }
 }
 
 if (arrLength > 1) {
-    console.log(secondLargest(intArray));
+  console.log(secondLargest(intArray));
 } else {
-    console.log(0);
+  console.log(0);
 }
