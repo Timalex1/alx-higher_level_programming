f1f92d3095fc0de3ed1e45263c986ac0b1bb21b1
Timalex1 <timilehinvincent@gmail.com>
2022-05-20 08:43:09 +0000

    fixing error

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@
 const myVar = process.argv.slice(2);
 
 if (myVar.length === 1) {
-    console.log(myVar[0] + ' is undefined');
+  console.log(myVar[0] + ' is undefined');
 } else if (myVar.length > 1) {
-    console.log(myVar[0] + ' is ' + myVar[1]);
+  console.log(myVar[0] + ' is ' + myVar[1]);
 } else {
-    console.log('undefined is undefined');
+  console.log('undefined is undefined');
 }
