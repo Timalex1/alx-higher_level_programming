b15a6f01a73618c5e6e74449260d50950eeb01d9
Timalex1 <timilehinvincent@gmail.com>
2022-05-20 08:55:39 +0000

    correcting errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ let i;
 const intRegex = /^[0-9]*?.[0-9]*$/;
 
 if (intRegex.test(process.argv[2])) {
-    for (i = 0; i <= (parseInt(process.argv[2]) - 1); i++) {
-	console.log('X'.repeat(parseInt(process.argv[2])));
-    }
+  for (i = 0; i <= (parseInt(process.argv[2]) - 1); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
